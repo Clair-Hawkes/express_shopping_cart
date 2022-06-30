0066a64590f60c314b6f019f024e5e6d96e888eb
LaithLou <laithlouay@gmail.com>
2022-06-29 20:12:31 -0400

    adding delete route

--- app.js
+++ app.js
@@ -2,51 +2,67 @@
 /** Simple demo shopping cart app. */
 
 const express = require("express");
-const {items} = require('./fakeDb');
+const { items } = require("./fakeDb");
 const app = express();
 
 // useful error class to throw
 const { NotFoundError, BadRequestError } = require("./expressError");
-
+const { response } = require("express");
 
 app.use(express.json());
 
 /** Returns JSON of all items in shopping cart
  * {items: [name: "popsicle", price: 1.45] } */
 app.get("/items", function (req, res) {
-  debugger
-  return res.json({items:items});
+  debugger;
+  return res.json({ items: items });
 });
 
 /** Accept JSON body, add item, and return item
  * {added: {name: "popsicle", price: 1.45}}:  */
-app.post('/items',function(req,res){
+app.post("/items", function (req, res) {
   // Catch the incoming JSON
   // Push to the Shopping Cart
   // Return {name: "popsicle", price: 1.45}}
-  debugger
+  debugger;
   const first = req.body;
   const recieved = req.body.name;
   items.push(req.body);
-  return res.json({"added":req.body});
-})
+  return res.json({ added: req.body });
+});
 
 /** Resturns JSON of single item in shopping cart, matching url param "name" */
-app.get('/items/:name',function(req,res){
+app.get("/items/:name", function (req, res) {
   // For of loop through the array
   // Checking for the name of object == req.params.name
   //Return that object
+  debugger;
+  for (let item of items) {
+    if (item.name === req.params.name) {
+      return res.json(item);
+    }
+  }
+});
 
+app.patch("/items/:name", function (req, res) {
+  debugger;
+  for (let item of items) {
+    if (item.name === req.params.name) {
+      item.price = req.body.price;
+      return res.json({ updated: item });
+    }
+  }
+});
 
-
-})
-
-
-
-
-
-
-
+app.delete("/items/:name", function (req, res) {
+  debugger;
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].name === req.params.name) {
+      items.splice(i, 1);
+      return res.json({ message: "Deleted", items: items });
+    }
+  }
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
